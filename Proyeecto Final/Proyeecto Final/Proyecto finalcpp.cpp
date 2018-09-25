ead105d089d5b9a0df69fd23adeb4dba306c2bcb
LAPTOP-PT2M9343\edgar <edgar_donato@outlook.com>
2018-09-24 20:12:44 -0500

    Madoficador activado y buscador por nombre y matricula listos!

--- Proyeecto Final/Proyeecto Final/Proyecto finalcpp.cpp
+++ Proyeecto Final/Proyeecto Final/Proyecto finalcpp.cpp
@@ -33,6 +33,7 @@ void Manual();
 void Buscar();
 void BuscarNombre();
 void BuscarMatricula();
+void Modificar();
 
 void main() {
 	system("cls");
@@ -42,7 +43,7 @@ void main() {
 
 	cout << "¿Que desea hacer?" << endl;
 
-	cout << "1. Ir a la lista de alumnos y calificaciones. \n2. Dar de alta alumnos. \n3. Buscar \n4. Guardar el archivo de texto. \n5. Ir al manual del usuario \n6. Salir. " << endl;
+	cout << "1. Ir a la lista de alumnos y calificaciones. \n2. Dar de alta alumnos. \n3. Buscar. \n4. Guardar el archivo de texto. \n5. Ir al manual del usuario. \n6. Modificar datos. \n7. Salir. " << endl;
 	
 	cin >> opcion;
 
@@ -68,10 +69,16 @@ void main() {
 		Manual();
 		break;
 
+	case 6:
+		Modificar();
+		break;
+
 	default:
 		break;
 
 	}
+
+	
   
 
 }
@@ -237,7 +244,42 @@ void Buscar() {
 void BuscarNombre() {
 	system("cls");
 
-	cout << "Escriba el nombre del alumno que desea buscar: ";
+	cin.ignore();
+
+	cout << "Escriba UNICAMENTE el nombre del alumno que desea buscar: ";
+
+	string n;
+	
+	getline(cin, n);
+
+	bool encontrado = false;
+
+	int i = 0;
+
+	while (i < c)
+	{
+		if (strcmp(n.c_str(), p[i].nombre.c_str()) == 0) { 
+
+			cout << "Lo encontré: " << endl;
+
+			cout << p[i].apellidoP << " " << p[i].apellidoM << " " << p[i].nombre << " Matricula: " << p[i].matricula << endl;
+
+			cout << "Calle: " << p[i].calle << " " << p[i].numCasa << ", " << p[i].colonia << endl;
+
+			cout << "Correo: " << p[i].correo << " Telefono:" << p[i].telefono << endl;
+
+			cout << "Calificacion 1: " << p[i].calificacion1 << " Calificacion 2: " << p[i].calificacion2 << " Calificacion 3: " << p[i].calificacion3 << endl << endl;
+			
+			encontrado = true;
+
+			break; 
+		}
+		i++;
+	}
+
+	if (!encontrado) {
+		cout << "No había registros con ese nombre" << endl;
+	}
 
 	cout << "1. Salir" << endl;
 
@@ -278,12 +320,226 @@ void BuscarMatricula() {
 			break;
 
 		}
-		if (!encontrado) {
-			cout << "No había registros con esa matricula." << endl;
-		}
+		
 
 		i++;
 	}
+
+	if (!encontrado) {
+		cout << "No había registros con esa matricula." << endl;
+	}
+	
+
+	cout << "1. Salir" << endl;
+
+	cin >> opcion;
+
+	if (opcion == 1) {
+		main();
+	}
+}
+
+void Modificar() {
+	
+	system("cls");
+
+	bool encontrado = false;
+
+	cout << "Escriba la matricula que desea modificar la informacion: ";
+
+	cin >> opcion;
+
+	for (int j = 0; j < c; j++)
+	{
+		if (opcion == p[j].matricula)
+		{
+			encontrado = true;
+			int i = 0;
+
+			while (i < c)
+			{
+				if (p[i].matricula == opcion) {
+
+					cout << "Encontre a: " << endl;
+
+					cout << endl;
+
+					cout << p[i].apellidoP << " " << p[i].apellidoM << " " << p[i].nombre << " Matricula: " << p[i].matricula << endl;
+
+					cout << "Calle: " << p[i].calle << " " << p[i].numCasa << ", " << p[i].colonia << endl;
+
+					cout << "Correo: " << p[i].correo << " Telefono:" << p[i].telefono << endl;
+
+					cout << "Calificacion 1: " << p[i].calificacion1 << " Calificacion 2: " << p[i].calificacion2 << " Calificacion 3: " << p[i].calificacion3 << endl << endl;
+
+					cout << "\n¿Qué deseas modificar?" << endl;
+
+					cout << "1. Nombre \n2. Apellidos \n3. Matricula \n4. Direccion  \n5. Correo \n6. Telefono \n7. Calificaciones " << endl;
+
+					cin >> opcion;
+
+
+					system("cls");
+
+
+					switch (opcion)
+					{
+					case 1:
+
+						cin.ignore();
+
+						cout << "Ingresa el nuevo nombre: ";
+
+						getline(cin, p[i].nombre);
+
+						break;
+
+					case 2:
+
+						cin.ignore();
+
+						cout << "Desea modificar apellido: " << endl;
+
+						cout << "1.Paterno \n2. Materno" << endl;
+
+						cin >> opcion;
+
+						system("cls");
+
+						if (opcion == 1)
+						{
+							cin.ignore();
+
+							cout << "Ingresa el nuevo apellido paterno: ";
+
+							getline(cin, p[i].apellidoP);
+
+
+						}
+						else
+						{
+							cin.ignore();
+
+							cout << "Ingresa el nuevo apellido materno: ";
+
+							getline(cin, p[i].apellidoM);
+						}
+
+						break;
+
+					case 3:
+
+						cout << "Ingresa la nueva matricula: ";
+
+						cin >> p[i].matricula;
+
+						break;
+
+					case 4:
+
+						cin.ignore();
+
+						cout << "Dato a modificar: " << endl;
+
+						cout << "1.Calle \n2. Numero de casa \n3. Colonia" << endl;
+
+						cin >> opcion;
+
+						system("cls");
+
+						if (opcion == 1) {
+
+							cin.ignore();
+
+							cout << "Ingresar nueva calle: " << endl;
+
+							getline(cin, p[i].calle);
+
+						}
+						else if (opcion == 2) {
+
+							cout << "Ingresar nuevo numero de casa: " << endl;
+
+							cin >> p[i].numCasa;
+
+						}
+						else {
+
+							cin.ignore();
+
+							cout << "Ingresar nueva colonia: " << endl;
+
+							getline(cin, p[i].colonia);
+						}
+
+						break;
+
+					case 5:
+						cin.ignore();
+
+						cout << "Ingresa el nuevo correo: ";
+
+						getline(cin, p[i].correo);
+
+						break;
+
+					case 6:
+
+						cin.ignore();
+
+						cout << "Ingresa el nuevo numero de telefono: ";
+
+						getline(cin, p[i].telefono);
+
+						break;
+
+					case 7:
+
+						cout << "Numero de calificacion a modificar: " << endl;
+
+						cin >> opcion;
+
+						system("cls");
+
+						if (opcion == 1)
+						{
+							cout << "Ingresa la nueva calificacion 1: ";
+
+							cin >> p[i].calificacion1;
+
+						}
+						else if (opcion == 2) {
+							cout << "Ingresa la nueva calificacion 2: ";
+
+							cin >> p[i].calificacion2;
+						}
+						else {
+							cout << "Ingresa la nueva calificacion 3: ";
+
+							cin >> p[i].calificacion3;
+						}
+
+
+
+					default:
+						break;
+					}
+
+				}
+
+
+				i++;
+
+			}
+
+		}
+	}
+
+	if (!encontrado) {
+		cout << "No había registros con esa matricula." << endl;
+	}
+
+	
 
 	
 
